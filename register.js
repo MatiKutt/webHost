0392e4236aa3afb4d5e74f1219c025f9bd5e1e7c
MatiKutt <58309985+MatiKutt@users.noreply.github.com>
2021-02-23 10:08:04 +0000

    pwa prog

--- register.js
+++ register.js
@@ -1,8 +1,9 @@
 window.onload = () => {
     'use strict';
-  
+	console.log("registered");
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
                .register('./service_worker.js');
+			   console.log("register function");
     }
   }
